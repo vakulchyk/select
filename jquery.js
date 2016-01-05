b4f96f0cd111fcdcb3219cb679af0271590529bd
vakulchyk <e.vakulchyk@gmail.com>
2016-01-05 14:13:05 +0700

    correct working with colors

--- jquery.js
+++ jquery.js
@@ -2,17 +2,10 @@ $(document).ready(function(){
     // wrap all selectors to add ::after in css
     $('select:not([disabled])').wrap('<label class="dropdown">');
     $('select[disabled]').wrap('<label class="dropdownDisabled">');
+
     // controlling color of elements
-    $('select').focusin(function(){
-        var thisDropdown= $(this);
-        if($(thisDropdown).prop('selectedIndex') == 0){
-            thisDropdown.css('color', '#555555');
-            thisDropdown.fcusout(function(){ 
-                if($(thisDropdown).prop('selectedIndex') == 0) {
-                    thisDropdown.css('color', '#c7cacb');
-                }
-            });
-        }
+    $("select").on("change", function() {
+        $(this).attr("data-value", $(this).val());
     });
 })
 
